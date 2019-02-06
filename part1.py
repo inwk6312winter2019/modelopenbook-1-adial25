b2def520ee3b4f1c8458edc48fb010e3f0c473b7
adial25 <Sri.AdilakshmiMulagala@dal.ca>
2019-02-06 14:18:04 -0400

    First Module

---
+++ part1.py
@@ -0,0 +1,17 @@
+def list_ipname_ip(address,netmask,name):
+	d={}
+	d[name]=(address,netmask)
+	return d
+
+l=[]
+fout=open("running-config.cfg")
+for interface in fout:
+	l.append(interface.split())
+	#print(l)
+for i in range(len(l)):
+	if l[i] =="nameif":
+		name=l[i+2]
+	elif l[i] == "ip":
+		address=l[i+3]
+		netmask=l[i++5]
+print(list_ipname_ip(address,netmask,name)) 
